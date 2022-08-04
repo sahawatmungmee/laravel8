a70f7a28b7439f9cffa770b9d110f18f05a165a8
sahawatmungmee <sahawat.mung@vru.ac.th>
2022-08-04 12:51:29 +0700

    w4

---
+++ resources/views/newgallery/bird.blade.php
@@ -0,0 +1,9 @@
+<html>
+
+<body>
+    <h1>Gallery of sahawat mungmee 63122420012 </h1>
+    <h1>Bird<a href="gallery/bird"><img src="{{$bird }} "></a></h1>
+
+</body>
+
+</html>
---
+++ resources/views/newgallery/cat.blade.php
@@ -0,0 +1,9 @@
+<html>
+
+<body>
+    <h1>Gallery of sahawat mungmee 63122420012 </h1>
+    <h1>Cat<a href="gallery/cat"><img src="{{$cat }} "></a></h1>
+
+</body>
+
+</html>
